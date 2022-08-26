d741f4c6765159233bc96ebdb010c87271bd06f2
Bai-Chiang <BaiChiang.Chiang@gmail.com>
2022-08-26 14:33:50 -0400

    separate homed.sh

--- arch_install.sh
+++ arch_install.sh
@@ -650,23 +650,25 @@ read -p "Do you want to create user with systemd-homed? [y/N] " IS_HOMED
 : "${IS_HOMED:=n}"
 IS_HOMED="${IS_HOMED,,}"
 if [ "$IS_HOMED" = y ] ; then
-    cat >> /mnt/root/homed.sh <<'EOF'
-systemctl enable --now systemd-homed.service
-read -p "Tell me your username: " USERNAME
-read -p "uid: (default 1000)" UID
-: "${UID:=1000}"
-read -p "Tell me the filesystem inside your home directory (btrfs or ext4): " FSTYPE
-homectl create "$USERNAME" --uid="$UID" --member-of=wheel --shell=/bin/bash --storage=luks --fs-type="$FSTYPE"
-
-read -p "Do you want to disable root account? [Y/n] " IS_ROOT_DISABLE
-: "${IS_ROOT_DISABLE:=n}"
-IS_ROOT_DISABLE="${IS_ROOT_DISABLE,,}"
-if [ "$IS_ROOT_DISABLE" = y ] ; then
-    # https://wiki.archlinux.org/title/Sudo#Disable_root_login
-    echo "Disabling root ..."
-    passwd -d root
-    passwd -l root
-EOF
+#    cat >> /mnt/root/homed.sh <<'EOF'
+#systemctl enable --now systemd-homed.service
+#read -p "Tell me your username: " USERNAME
+#read -p "uid: (default 1000)" UID
+#: "${UID:=1000}"
+#read -p "Tell me the filesystem inside your home directory (btrfs or ext4): " FSTYPE
+#homectl create "$USERNAME" --uid="$UID" --member-of=wheel --shell=/bin/bash --storage=luks --fs-type="$FSTYPE"
+#
+#read -p "Do you want to disable root account? [Y/n] " IS_ROOT_DISABLE
+#: "${IS_ROOT_DISABLE:=n}"
+#IS_ROOT_DISABLE="${IS_ROOT_DISABLE,,}"
+#if [ "$IS_ROOT_DISABLE" = y ] ; then
+#    # https://wiki.archlinux.org/title/Sudo#Disable_root_login
+#    echo "Disabling root ..."
+#    passwd -d root
+#    passwd -l root
+#fi
+#EOF
+    cp "$(pwd)/homed.sh" /mnt/root/homed.sh
     echo "Created /root/homed.sh Run it after reboot to create systemd-homed user, which can't be done in chroot environment."
     echo "Enter root password (/root/homed.sh can disable root account after you setup systemd-homed user)"
     arch-chroot /mnt passwd
---
+++ homed.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+systemctl enable --now systemd-homed.service
+read -p "Tell me your username: " USERNAME
+read -p "uid: (default 1000)" UID
+: "${UID:=1000}"
+read -p "Tell me the filesystem inside your home directory (btrfs or ext4): " FSTYPE
+homectl create "$USERNAME" --uid="$UID" --member-of=wheel --shell=/bin/bash --storage=luks --fs-type="$FSTYPE"
+
+read -p "Do you want to disable root account? [Y/n] " IS_ROOT_DISABLE
+: "${IS_ROOT_DISABLE:=n}"
+IS_ROOT_DISABLE="${IS_ROOT_DISABLE,,}"
+if [ "$IS_ROOT_DISABLE" = y ] ; then
+    # https://wiki.archlinux.org/title/Sudo#Disable_root_login
+    echo "Disabling root ..."
+    passwd -d root
+    passwd -l root
+fi
