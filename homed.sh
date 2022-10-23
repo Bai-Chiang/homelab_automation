cfed9098a163111b953547c7ed6baaac888b8b01
Bai-Chiang <contact@bai-chiang.com>
2022-10-23 16:50:35 -0400

    fixed UID

--- homed.sh
+++ homed.sh
@@ -2,10 +2,10 @@
 
 systemctl enable --now systemd-homed.service
 read -p "Tell me your username: " USERNAME
-read -p "uid: (default 1000)" UID
-: "${UID:=1000}"
+read -p "uid: (default 1000)" HOMED_UID
+: "${HOMED_UID:=1000}"
 read -p "Tell me the filesystem inside your home directory (btrfs or ext4): " FSTYPE
-homectl create "$USERNAME" --uid="$UID" --member-of=wheel --shell=/bin/bash --storage=luks --fs-type="$FSTYPE"
+homectl create "$USERNAME" --uid="$HOMED_UID" --member-of=wheel --shell=/bin/bash --storage=luks --fs-type="$FSTYPE"
 
 read -p "Do you want to disable root account? [Y/n] " IS_ROOT_DISABLE
 : "${IS_ROOT_DISABLE:=n}"
